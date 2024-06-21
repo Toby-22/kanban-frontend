5061785bb71019053ae0914d3ed7deaa684c8d29
Tobias <112898220+Toby-22@users.noreply.github.com>
2024-06-21 16:34:34 +0200

    add task filter for status columns

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import {MatCardModule} from '@angular/material/card';
 import { FormsModule } from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
@@ -13,6 +13,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import { BoardComponent } from './components/board/board.component';
+import { AuthIntercepterService } from './services/auth-intercepter.service';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { BoardComponent } from './components/board/board.component';
     MatIconModule,
   ],
   providers: [
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    { provide: HTTP_INTERCEPTORS, useClass: AuthIntercepterService, multi: true }
   ],
   bootstrap: [AppComponent]
 })
--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { enviroment } from '../../../enviroments/enviroments';
+import { lastValueFrom } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+interface Task{
+    "id": number,
+    "title": string,
+    "description": string,
+    "created_at": Date,
+    "due_date": Date,
+    "status": string,
+    "category": string,
+    "assignee": number
+}
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrl: './board.component.scss'
+  styleUrl: './board.component.scss',
 })
-export class BoardComponent {
+export class BoardComponent implements OnInit{
+  tasks: Task[] = [];
+  todoTasks: Task[] = [];
+  inWorkTasks: Task[] = [];
+  inReviewTasks: Task[]= [];
+  doneTasks: Task[] = [];
+  error = '';
 
+  constructor(private http: HttpClient){}
+
+  async ngOnInit() {
+      try{
+        this.tasks = await this.loadTasks();
+        console.log(this.tasks);
+        this.filterTasks();
+      }catch(e){
+        this.error = "Fehler beim laden";
+      }
+  }
+
+  loadTasks(){
+    const url = enviroment.apiUrl + '/tasks/';
+    return lastValueFrom(this.http.get<Task[]>(url));
+  }
+
+  filterTasks() {
+    this.todoTasks = this.tasks.filter(task => task.status === 'todo');
+    this.inWorkTasks = this.tasks.filter(task => task.status === 'inWork');
+    this.inReviewTasks = this.tasks.filter(task => task.status === 'inReview');
+    this.doneTasks = this.tasks.filter(task => task.status === 'done');
+  }
+
+  // getTasksByStatus(status: string){
+  //   return this.tasks.filter(task => task.status === status)
+  // }
 }
